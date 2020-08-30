45a24fbf296be1bf9d140665d7ce9d5ac78d01cc
Bhaskar Verma <bhaskarverma.hacked00@gmail.com>
2020-08-30 14:15:49 +0530

    3008202010

---
+++ modules/Broken Vehicles/manifest.php
@@ -0,0 +1,12 @@
+<?php
+
+$details = array();
+
+$details['list_type'] = 'single';
+$details['pages_to_list'] = ['Vehicles' => 'vehicles.php'];
+$details['pages_all'] = ['Vehicles' => 'vehicles.php'];
+$details['version'] = 1.0;
+
+return $details;
+
+?>
