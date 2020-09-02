59efcec938c84c8ff1193c8d007d5f6a0b0ac5a4
Bhaskar Verma <bhaskarverma.hacked00@gmail.com>
2020-09-03 02:36:42 +0530

    Update main_sidebar.php

--- modules/core/sidebar/main_sidebar.php
+++ modules/core/sidebar/main_sidebar.php
@@ -49,7 +49,7 @@ for($i=0; $i<count($modules_in_db); $i++)
   array_push($modules, $modules_in_db[$i]);
 }
 
-var_dump($modules_in_db);
+var_dump($modules);
 
 //Checking for Difference in Module Lists
 $modules_not_in_db = array(); //Array to contain the list of Recently Added Modules
